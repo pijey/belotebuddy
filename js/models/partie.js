03b14abecfb6784027960d057a44562508f22ea2
Pierre-Julien Villoud <pierre-julien.villoud@atos.net>
2014-09-26 17:04:23 +0200

    ajout du formattage de la date

--- js/models/partie.js
+++ js/models/partie.js
@@ -4,6 +4,9 @@ App.Partie = DS.Model.extend({
 	nomNS: attr("string", {defaultValue: "Nord-Sud"}),
     nomEO: attr("string", {defaultValue: "Est-Ouest"}),
     date: attr('date'),
+    formatedDate: function(){
+        return moment(this.get('date')).format('DD/MM/YYYY HH:mm:ss');
+    }.property('date'),
 	scoreNS: function(){
         var donnes = this.get("donnes");
         return donnes.reduce(function(previousValue, donne){
